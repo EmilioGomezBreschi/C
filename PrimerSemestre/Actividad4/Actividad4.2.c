77d8c7920a89c0f4c5908b8ce5e149f19a0aa1dc
EmilioGomezBreschi <emilio.gomez7881@alumnos.udg.mx>
2024-10-07 07:56:28 -0600

    Completada la Actividad 4

---
+++ PrimerSemestre/Actividad4/Actividad4.2.c
@@ -0,0 +1,18 @@
+/*Escribe una función en C llamada incrementarPorReferencia que tome un puntero a un entero como argumento, incremente el valor al que apunta en 5 unidades y luego imprima el resultado dentro de la función. En la función main(), declara una variable entera, llama a la función incrementarPorReferencia() pasando un puntero a la variable como argumento y muestra el valor original y el valor modificado después de llamar a la función.*/
+
+#include <stdio.h>
+
+void incrementarPorReferencia(int *a)
+{
+  *a += 5;
+  printf("El valor dentro de la funcion es: %d\n", *a);
+}
+
+int main()
+{
+  int a = 10;
+  printf("El valor original es: %d\n", a);
+  incrementarPorReferencia(&a);
+  printf("El valor despues de la funcion es: %d\n", a);
+  return 0;
+}
