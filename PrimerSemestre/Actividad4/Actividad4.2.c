2efa962670328b21f6f121148b41061915ef664f
EmilioGomezBreschi <emilio.gomez7881@alumnos.udg.mx>
2024-10-09 07:50:19 -0600

    Cambio al nombre de las carpetas

--- PrimerSemestre/Actividad4/Actividad4.2.c
+++ PrimerSemestre/Actividad4/Actividad4.2.c
@@ -1,4 +1,4 @@
-/*Escribe una función en C llamada incrementarPorReferencia que tome un puntero a un entero como argumento, incremente el valor al que apunta en 5 unidades y luego imprima el resultado dentro de la función. En la función main(), declara una variable entera, llama a la función incrementarPorReferencia() pasando un puntero a la variable como argumento y muestra el valor original y el valor modificado después de llamar a la función.*/
+/* Escribe una función en C llamada incrementarPorReferencia que tome un puntero a un entero como argumento, incremente el valor al que apunta en 5 unidades y luego imprima el resultado dentro de la función. En la función main(), declara una variable entera, llama a la función incrementarPorReferencia() pasando un puntero a la variable como argumento y muestra el valor original y el valor modificado después de llamar a la función.*/
 
 #include <stdio.h>
 
